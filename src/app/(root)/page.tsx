18924b7dd9a9b9d51adb884910bd97deb1aadb69
Cao Nam <nam52871@donga.edu.vn>
2024-03-02 23:09:44 +0700

    Setup routes

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="h-96 container ">
+      {/* <h1 className="font-bold text-[50px] capitalize mt-5 w-[50vw] mx-auto text-center">
+        Elevate your every step with trendsetting{" "}
+        <span className="text-stroke-3 text-white">footwear .</span>
+      </h1>
+      <p className="capitalize text-center w-[80vh] mx-auto text-sm ">
+        Step intp style and comfort with our trendsetting footwear collection,
+        Where every stride is a fashionable indulgence
+      </p> */}
+    </main>
+  );
+}
