e0320e64ae5ac0c886e4c6fbaaf22d770904911b
Cao Nam <nam52871@donga.edu.vn>
2024-03-05 00:50:01 +0700

    Manage register form with react form hook and yup

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,42 +1,134 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Underline from "@/components/ui/underline";
 import { path } from "@/constants/path";
+import { AuthSchema, authSchema } from "@/lib/rules";
+import { yupResolver } from "@hookform/resolvers/yup";
 import Link from "next/link";
 import React from "react";
+import { useForm } from "react-hook-form";
 import { FaFacebook, FaGoogle } from "react-icons/fa";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { cn } from "@/lib/utils";
 
-export default function page() {
+export default function Page() {
+  const form = useForm<AuthSchema>({ resolver: yupResolver(authSchema) });
+
+  async function onSubmit(values: AuthSchema) {
+    console.log(values);
+  }
+
   return (
     <div className="mt-0 p-10">
       <div className="text-xl font-medium">Register</div>
-      <p className="text-sm mt-2 text-gray-500">
+      <p className="text-sm mt-2 text-gray-500 mb-6">
         Sign in to your account for a better shopping experience
       </p>
-      <div className="mt-7">
-        <h1 className="mb-3 text-sm">Email Address*</h1>
-        <Input />
-      </div>
-      <div className="mt-7">
-        <h1 className="mb-3 text-sm">Username*</h1>
-        <Input />
-      </div>
-      <div className="flex gap-6">
-        <div className="mt-7 w-[60%]">
-          <h1 className="mb-3 text-sm">Password*</h1>
-          <Input />
-        </div>
-        <div className="mt-7  w-[40%]">
-          <h1 className="mb-3 text-sm">Confirm Password*</h1>
-          <Input />
-        </div>
-      </div>
-      <Button className="w-full mt-7" size="lg">
-        Register
-      </Button>
+      <fieldset>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={cn("mb-3 text-sm font-normal")}>
+                    Email*
+                  </FormLabel>
+                  <FormControl className="">
+                    <Input
+                      {...field}
+                      className={cn(
+                        form.formState.errors.email && "border-red-300"
+                      )}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-sm font-normal text-[13px]" />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={cn("mb-3 text-sm font-normal")}>
+                    Name*
+                  </FormLabel>
+                  <FormControl className="">
+                    <Input
+                      {...field}
+                      className={cn(
+                        form.formState.errors.email && "border-red-300"
+                      )}
+                    />
+                  </FormControl>
+                  <FormMessage className="text-sm font-normal text-[13px]" />
+                </FormItem>
+              )}
+            />
+            <div className="flex gap-5">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem className="w-[60%]">
+                    <FormLabel className={cn("text-sm font-normal")}>
+                      Password*
+                    </FormLabel>
+                    <FormControl className="">
+                      <Input
+                        type="password"
+                        {...field}
+                        className={cn(
+                          form.formState.errors.password && "border-red-300"
+                        )}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-sm font-normal text-[13px]" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirm_password"
+                render={({ field }) => (
+                  <FormItem className="">
+                    <FormLabel className={cn("text-sm font-normal")}>
+                      Comfirm password*
+                    </FormLabel>
+                    <FormControl className="">
+                      <Input
+                        type="password"
+                        {...field}
+                        className={cn(
+                          form.formState.errors.password && "border-red-300"
+                        )}
+                      />
+                    </FormControl>
+                    <FormMessage className="text-sm font-normal text-[13px]" />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <Button type="submit" className="w-full" size="lg">
+              Create Account
+            </Button>
+          </form>
+        </Form>
+      </fieldset>
       <div className="flex items-center gap-5 mt-6 opacity-45">
         <div className="flex-1 h-[0.25px] bg-gray-500"></div>
-        <div className="text-[12px]">Or login with</div>
+        <div className="text-[12px]">Or register with</div>
         <div className="flex-1 h-[0.25px] bg-gray-500"></div>
       </div>
       <div className="flex gap-5 mt-6">
